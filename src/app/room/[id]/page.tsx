fa5e33604f8a2a75236ae812d89a4e1561d00c35
Andreas Gassmann <andreas@andreasgassmann.ch>
2025-04-15 21:13:54 +0200

    feat: pageprops

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -3,6 +3,12 @@ import RoomClient from "./RoomClient";
 // Disable static generation for this route
 export const dynamic = "force-dynamic";
 
-export default function RoomPage({ params }: { params: { id: string } }) {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function RoomPage({ params }: PageProps) {
   return <RoomClient roomId={params.id} />;
 }
